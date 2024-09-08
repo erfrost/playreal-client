061c3858abec26e107778a0b7c43dd77aef4f982
Дмитрий <dmitriy@MacBook-Air-Dmitrij.local>
2024-09-08 12:43:03 +0500

    autodeploy

--- environments.ts
+++ environments.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://147.45.168.75:8000/api/";
-export const BASE_SOCKET_URL = "http://147.45.168.75:8000/";
-// export const BASE_URL = "http://localhost:8000/api/";
-// export const BASE_SOCKET_URL = "http://localhost:8000/";
+// export const BASE_URL = "http://147.45.168.75:8000/api/";
+// export const BASE_SOCKET_URL = "http://147.45.168.75:8000/";
+export const BASE_URL = "http://localhost:8000/api/";
+export const BASE_SOCKET_URL = "http://localhost:8000/";
